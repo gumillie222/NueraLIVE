d3ce8967ab7fb9049a6e9e7de9b647153f34db7b
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2023-10-21 16:20:35 -0400

    updated threshold for branch

--- count_branch.py
+++ count_branch.py
@@ -5,37 +5,15 @@ import cv2
 import glob
 import numpy as np
 
-# image = cv2.imread('./SHSY5Y_Phase_B10_1_00d04h00m_4.tif')
-
-
-#plt.figure(figsize=(10, 5))
-
-# Display the original image
-#plt.subplot(1, 2, 1)
-#plt.imshow(image, cmap='gray')
-#plt.title('Original Image')
-#plt.axis('off')
-
-# Display the filtered image
-#plt.subplot(1, 2, 2)
-#plt.imshow(filtered_image, cmap='gray')
-#plt.title('Filtered Image (Frangi)')
-#plt.axis('off')
-#plt.show()
-
-
 
 def count_branches(image):
     greyscale = cv2. cvtColor(image, cv2.COLOR_BGR2GRAY)
     dns_greyscale = cv2.fastNlMeansDenoising(greyscale)
     filtered_image  = meijering(dns_greyscale, [1], black_ridges = True)
     
-    return np.sum((filtered_image > 5/255) & (filtered_image < 0.045))
+    return np.count_nonzero((filtered_image > 2/255) & (filtered_image < 0.05))
 
 
-    # image[((filtered_image > 5/255) & (filtered_image < 0.045))] = (255, 0, 0)
-
-    # filtered_image = np.where(filtered_image > 100/255, 0, filtered_image)
 
 # Process the images
 image_files = glob.glob('./SHSY5Y_Rep1/*.tif')
@@ -44,18 +22,15 @@ results = [[],[],[],[]]
 # Extract image numbers from filenames
 image_numbers = [int(''.join(filter(lambda x: x in '0123456789', image_file.split('_')[5]))) for image_file in image_files]
 image_culture_numbers = [int(''.join(filter(lambda x: x in '0123456789', image_file.split('_')[6]))) for image_file in image_files]
-print(image_numbers)
-print(image_culture_numbers)
+
 # Sort the image files based on the image number
 sorted_image_files = [x for _, x in sorted(zip(image_numbers, zip(image_files, image_culture_numbers)))]
-print(sorted_image_files)
 
 for image_file, image_culture_number in sorted_image_files:
      image = cv2.imread(image_file, cv2.IMREAD_COLOR)
      num_branches = count_branches(image)
      if (image_culture_number in [1, 2, 3, 4]):
         results[image_culture_number-1].append(num_branches)
-
 
 
 plt.scatter(range(1, len(results[0]) + 1), results[0], marker='o', s=30, c='b', label='Culture 1')
